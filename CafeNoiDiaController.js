0c38dff4fae2e869105618081e6291c1bfa5870d
trungvnimm <trungnv.vnimm@gmail.com>
2018-02-22 16:04:49 +0700

    update document success

--- CafeNoiDiaController.js
+++ CafeNoiDiaController.js
@@ -12,33 +12,44 @@ router.get('/', function (req, res) {
     });
     
 });
-// Tự động chạy
+// Tự động chạy mỗi giờ
 var cron = require('node-cron');
 var request = require('request');
 var cheerio = require('cheerio');
 var mongoose = require('mongoose');
-cron.schedule('34 14 * * *', function(){
+cron.schedule('0 */59 * * * *', function(){
     console.log('running a task every minute');
     request('https://giacaphe.com', function (error, response, html) {
     if (!error && response.statusCode == 200) {
         var $ = cheerio.load(html);
         var json = [];
         $('#gianoidia tr').each(function (i, e) {
-            //remove first row
+            //xóa hàng đầu tiên
             if(i != 0){
                 json.push({
                     ten_thi_truong: $(this).children().first().text(),
                     gia: $(this).children().eq(1).first().text(),
                     thay_doi: $(this).children().eq(2).first().text()
                 })
+                
             }
         });
         console.log(JSON.stringify(json));
-        // Duyệt list và Insert vào database mongodb 
+       
+        // Cap nhat vao database
+        // Update 
         for(var item in json){
-            new CafeNoiDia(json[item]).save().catch((err)=>{
-                console.log(err.message);
-            });
+            var newItem = json[item];
+            var conditions = {'ten_thi_truong': newItem.ten_thi_truong};
+            //console.log("ITEM: " + newItem +"---");
+            var options = {upsert: true};
+            CafeNoiDia.update(conditions, newItem, options, 
+                function(err, doc){
+                    if(err){
+                        console.log("Something wrong when updating data! "+err);
+                    }
+                    console.log(doc);
+                });
         }
     }
     })
